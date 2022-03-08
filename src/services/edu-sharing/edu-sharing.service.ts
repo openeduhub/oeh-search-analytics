df2d22ea1d67f3aeac2dfbbec24d20a1d612961b
Christopher Lübbemeier <luebbemeier@metaventis.com>
2022-03-08 17:50:24 +0100

    fix: report click to edu-sharing

--- src/services/edu-sharing/edu-sharing.service.ts
+++ src/services/edu-sharing/edu-sharing.service.ts
@@ -1,4 +1,4 @@
-import { HttpService, Injectable } from '@nestjs/common';
+import { HttpService, Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
@@ -8,6 +8,7 @@ export class EduSharingService {
         'https://redaktion.openeduhub.net/edu-sharing',
     );
     private readonly restUrl = this.eduSharingUrl + '/rest';
+    private readonly logger = new Logger(EduSharingService.name);
 
     constructor(
         private readonly configService: ConfigService,
@@ -15,16 +16,20 @@ export class EduSharingService {
     ) {}
 
     async reportView(clickedResult: any): Promise<void> {
-        if (clickedResult.lom.technical.location.startsWith(this.eduSharingUrl)) {
+        if (!clickedResult.properties['ccm:wwwurl']) {
             // Don't report views of Edu-Sharing content since Edu-Sharing tracks these itself.
             return;
         }
-        const repository = '-home-';
         const event = 'VIEW_MATERIAL';
+        this.logger.log(`Reporting click to ${this.restUrl}`);
         await this.httpService
-            .put(this.restUrl + `/tracking/v1/tracking/${repository}/${event}`, null, {
-                params: { node: clickedResult.id },
-            })
+            .put(
+                this.restUrl + `/tracking/v1/tracking/${clickedResult.ref.repo}/${event}`,
+                {},
+                {
+                    params: { node: clickedResult.ref.id },
+                },
+            )
             .toPromise();
     }
 }
